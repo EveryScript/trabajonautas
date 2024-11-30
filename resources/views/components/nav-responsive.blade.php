c41cfae1309e88c1e26822022a0254a6103ba680
EveryScript <everquispeticona@gmail.com>
2024-11-30 11:43:01 -0400

    style(delete dark mode navbar web)

--- resources/views/components/nav-responsive.blade.php
+++ resources/views/components/nav-responsive.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="bg-white border-gray-200">
     <div x-data="{ open: false, dropdown: false }" @click.away="open = false, dropdown = false"
         class="max-w-6xl flex flex-wrap items-center justify-between mx-auto p-4 relative">
         <x-nav-logo />
@@ -15,7 +15,7 @@
 
                 <!-- Dropdown menu -->
                 <div x-show="open"
-                    class="absolute top-5 right-0 z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                    class="absolute top-5 right-0 z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
                     id="user-dropdown">
                     <div class="px-4 py-3">
                         <span class="block text-sm text-gray-900">{{ Auth::user()->name }}</span>
@@ -55,7 +55,7 @@
                 @role('USER|ADMIN')
                     <!-- Hamburger -->
                     <button @click="dropdown = !dropdown" data-collapse-toggle="navbar-user" type="button"
-                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                         aria-controls="navbar-user" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -71,7 +71,7 @@
             <div x-bind:class="dropdown ? '' : 'hidden'"
                 class="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-user">
                 <ul
-                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                     @role('USER|ADMIN')
                         <x-nav-link-responsive href="{{ route('dashboard') }}" wire:navigate :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
