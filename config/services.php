69c2be94175a7afda1f353d40a1601d4bf0e0e5c
Ever Quispe <everquispeticona@email.com>
2026-07-15 02:05:26 -0400

    feat(Baneco QR payment method, many files affected with functionality)

--- config/services.php
+++ config/services.php
@@ -40,8 +40,16 @@ return [
     'google_analytics' => [
         'id' => env('GOOGLE_ANALYTICS_ID')
     ],
-    // VAPID Clave Pública de Firebase para notificaciones
+    // VAPID Public Notification Firebace Key
     'firebase' => [
         'vapid_key' => env('FIREBASE_VAPID_KEY'),
-    ]
+    ],
+    // Baneco Service
+    'baneco' => [
+        'url'      => env('BANECO_URL'),
+        'username' => env('BANECO_USERNAME'),
+        'password' => env('BANECO_PASSWORD'),
+        'aes_key'  => env('BANECO_AES_KEY'),
+        'account'  => env('BANECO_ACCOUNT'),
+    ],
 ];
