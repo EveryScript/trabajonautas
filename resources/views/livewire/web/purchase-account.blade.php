2128bcae13f133e00bf8826f4b7ddcd0a8978638
Ever Quispe <everquispeticona@email.com>
2026-08-13 12:09:57 -0400

    fix(Update confimAndSave method in purchase-account.php adding generateQR service from Baneco and set appearence in renew form)

--- resources/views/livewire/web/purchase-account.blade.php
+++ resources/views/livewire/web/purchase-account.blade.php
@@ -20,9 +20,9 @@
                 <span class="block mb-2 text-xs text-tbn-dark dark:text-tbn-light">
                     Revisa tus datos y escanea el código QR para realizar tu depósito.</span>
                 <div class="flex flex-col w-full gap-6 md:flex-row">
-                    <div class="w-full text-sm md:w-3/5">
+                    <div class="w-full text-sm">
                         <section
-                            class="max-w-2xl mx-auto mb-4 overflow-hidden bg-white border shadow-sm dark:bg-tbn-dark rounded-2xl border-tbn-light dark:border-tbn-secondary">
+                            class="mx-auto mb-4 overflow-hidden bg-white border shadow-sm dark:bg-tbn-dark rounded-2xl border-tbn-light dark:border-tbn-secondary">
                             <div class="px-6 py-4">
                                 <table class="w-full border-collapse">
                                     <tbody class="divide-y divide-tbn-light dark:divide-tbn-secondary">
@@ -125,66 +125,12 @@
                         </section>
 
                     </div>
-                    <div class="w-full text-sm md:w-2/5">
-                        @if ($account_type_id == 2)
-                            <picture class="block max-w-[10rem] mx-auto mb-4">
-                                <img class="w-full rounded-lg" src="{{ asset('storage/' . $qr_pro->value) }}"
-                                    alt="qr-code">
-                            </picture>
-                            <div class="mb-8 text-center">
-                                <a href="{{ asset('storage/' . $qr_pro->value) }}" download
-                                    class="inline-block px-3 py-2 text-xs transition-all duration-200 border rounded-full text-tbn-primary border-tbn-primary hover:bg-tbn-primary hover:text-white">
-                                    Descargar QR
-                                </a>
-                            </div>
-                        @endif
-                        @if ($account_type_id == 3)
-                            <picture class="block max-w-[10rem] mx-auto mb-4">
-                                <img class="w-full rounded-lg" src="{{ asset('storage/' . $qr_promax->value) }}"
-                                    alt="qr-code">
-                            </picture>
-                            <div class="mb-8 text-center">
-                                <a href="{{ asset('storage/' . $qr_promax->value) }}" download
-                                    class="inline-block px-3 py-2 text-xs transition-all duration-200 border rounded-full text-tbn-primary border-tbn-primary hover:bg-tbn-primary hover:text-white">
-                                    Descargar QR
-                                </a>
-                            </div>
-                        @endif
-                        <div
-                            class="flex items-center justify-between max-w-sm p-4 mb-4 transition-colors bg-white border shadow-sm dark:bg-tbn-dark border-tbn-light dark:border-tbn-secondary rounded-xl">
-                            <div class="flex items-center gap-4">
-                                <div>
-                                    <h4 class="text-sm font-semibold text-tbn-secondary dark:text-tbn-light">
-                                        Banco Mercantil Santa Cruz</h4>
-                                    <p x-text="bankAccount"
-                                        class="font-mono text-xl tracking-wider text-tbn-dark dark:text-white"></p>
-                                </div>
-                            </div>
-                            <button x-on:click="copyClipboardBankAccount"
-                                class="p-2 rounded-full hover:text-tbn-primary text-tbn-secondary">
-                                <svg x-show="!copied" class="w-5 h-5" fill="none" stroke="currentColor"
-                                    viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z">
-                                    </path>
-                                </svg>
-                                <svg x-show="copied" x-cloak class="w-5 h-5" fill="none" stroke="currentColor"
-                                    viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M5 13l4 4L19 7"></path>
-                                </svg>
-                            </button>
-                        </div>
-                        <p class="text-xs text-tbn-dark dark:text-tbn-light">
-                            Una vez realizado el depósito nuestros operadores se comunicarán contigo para confirmar el
-                            depósito y habilitar tu cuenta.</p>
-                    </div>
                 </div>
                 <div class="flex justify-between gap-1 mt-4">
                     <x-secondary-button type="button" wire:click='backToDashboard'>
                         Volver al panel</x-secondary-button>
                     <x-button type="button" wire:click="confirmAndSave" wire:loading.attr='confirmAndSave'>
-                        <span wire:loading.remove wire:target="confirmAndSave">Confirmar</span>
+                        <span wire:loading.remove wire:target="confirmAndSave">Generar QR</span>
                         <span wire:loading wire:target="confirmAndSave">
                             <i class="text-sm fas fa-spinner animate-spin"></i></span>
                     </x-button>
